ff8f6426c98de5aafb0fbb7cf2584883c6ade2a3
Darrin Stephens <d.stephens@appliedccm.com.au>
2019-04-21 09:53:34 +1000

    Update script to python3, dropping python 2 compatibility.

--- bin/cleanLnInclude.py
+++ bin/cleanLnInclude.py
@@ -17,8 +17,8 @@ eg: %s OpenFOAM
 try:
     opts, args = getopt.getopt(sys.argv[1:], "hs", ['help','silent'])
 except getopt.GetoptError as err:
-    print err 
-    print Usage
+    print (err) 
+    print (Usage)
     sys.exit()
 
 # initialise default values for arguments
@@ -26,7 +26,7 @@ silent = False
 
 for o, a in opts:
     if o in ('-h', '--help'):
-        print Usage
+        print (Usage)
         sys.exit()
     elif o in ('-s', "--silent"):
         silent = True
@@ -37,7 +37,7 @@ for o, a in opts:
 if  (len(args) > 0) and (len(args) < 2):
     dir = args[0]
 else:
-    print "No directory or too many directories given"
+    print ("No directory or too many directories given")
     sys.exit()
 	
 # Determine if we are Windows or POSIX	
@@ -55,11 +55,11 @@ else:
     path = os.getcwd() + sl + dir
 
 if not (silent == True):
-  print "Removing lnInclude directories: " + dir
+  print ("Removing lnInclude directories: " + dir)
 
 for (dirpath, dirnames, filenames) in os.walk(path):
     for dirname in dirnames:
         if (dirname == 'lnInclude'):
             shutil.rmtree(os.path.join(dirpath, dirname))
             if not (silent == True):
-                print 'Removing directory '+ os.path.join(dirpath, dirname)
+                print ('Removing directory '+ os.path.join(dirpath, dirname))
